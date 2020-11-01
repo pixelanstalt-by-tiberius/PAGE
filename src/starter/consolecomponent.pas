7d0785038f7d445d2403ca51918d5ca781f44846
tiberius <info@pixelanstalt.net>
2020-11-01 14:46:25 +0100

    Add new console component for debug console

---
+++ src/starter/consolecomponent.pas
@@ -0,0 +1,176 @@
+unit ConsoleComponent;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LazUTF8, Graphics, Math;
+
+type
+
+  { TConsoleOutput }
+
+  TConsoleOutput = class
+  private
+
+  protected
+    FBackgroundColor: TColor;
+    FBackgroundStyle: TBrushStyle;
+
+    FTextBuffer: TStringList;
+    FTextColor, FTextErrorColor, FTextWarningColor: TColor;
+    FHighlight: Integer;
+
+    FCanvas: TCanvas;
+    FHeight, FWidth: Integer;
+
+    FInputBuffer: String;
+
+    procedure DrawBackground;
+    procedure DrawTextBuffer;
+    procedure DrawInputLine;
+
+    procedure EncodeAndDrawLine(X, Y, Line: Integer);
+    procedure ProcessCommand(Command: String);
+  public
+    constructor Create(ACanvas: TCanvas; AWidth, AHeight: Integer);
+    destructor Free;
+    procedure DoDraw;
+
+    property Canvas: TCanvas read FCanvas write FCanvas;
+    property Buffer: TStringList read FTextBuffer;
+    property Height: Integer read FHeight write FHeight;
+    property Width: Integer read FWidth write FWidth;
+    property InputBuffer: String read FInputBuffer write FInputBuffer;
+  end;
+
+implementation
+
+{ TConsoleOutput }
+
+procedure TConsoleOutput.DrawBackground;
+begin
+  FCanvas.Brush.Color := FBackgroundColor;
+  FCanvas.Brush.Style := FBackgroundStyle;
+  FCanvas.FillRect(1, 1, FWidth, FHeight);
+end;
+
+procedure TConsoleOutput.DrawTextBuffer;
+var
+  intLoop, intLineOffset: Integer;
+  intLinesPerScreen: Integer;
+begin
+  FCanvas.Brush.Style := bsClear;
+  FCanvas.Font.Color := FTextColor;
+
+  intLinesPerScreen := Floor(FHeight/(FCanvas.Font.Size+3))-1;
+
+  if FTextBuffer.Count > intLinesPerScreen then
+    intLineOffset := FTextBuffer.Count-intLinesPerScreen
+  else
+    intLineOffset := 0;
+
+  for intLoop := intLineOffset to FTextBuffer.Count-1 do
+    EncodeAndDrawLine(1, ((intLoop-intLineOffset)*(FCanvas.Font.Size+3)), intLoop);
+end;
+
+procedure TConsoleOutput.DrawInputLine;
+begin
+  FCanvas.Pen.Color := clSilver;
+  FCanvas.Line(1, FHeight-(FCanvas.Font.Size+8), FWidth, FHeight-(FCanvas.Font.Size+8));
+  FCanvas.TextOut(1, FHeight-(FCanvas.Font.Size+7), '$>' + FInputBuffer + '▁');
+end;
+
+procedure TConsoleOutput.EncodeAndDrawLine(X, Y, Line: Integer);
+var
+  strLeft: String;
+  intEscapePos: Integer;
+  intCurrentXPos: Integer;
+  intCMDEndPos: Integer;
+begin
+  strLeft := FTextBuffer[Line];
+  intCurrentXPos := X;
+  repeat
+    intEscapePos := UTF8Pos('\', strLeft);
+    if intEscapePos = 1 then
+    begin
+      intCMDEndPos := UTF8Pos(' ', strLeft, intEscapePos);
+      if intCMDEndPos = 0 then
+        intCMDEndPos := UTF8Length(strLeft)+1;
+      ProcessCommand(UTF8Copy(strLeft, intEscapePos+1,
+          intCMDEndPos-intEscapePos-1));
+      if intCMDEndPos <> UTF8Length(strLeft) then
+        strLeft := UTF8Copy(strLeft, intCMDEndPos+1,
+          UTF8Length(strLeft)-intCMDEndPos+1)
+      else
+        strLeft := '';
+    end
+    else
+    if intEscapePos = 0 then
+    begin
+      FCanvas.TextOut(intCurrentXPos, Y, strLeft);
+      strLeft := '';
+    end
+    else
+    if intEscapePos > 1 then
+    begin
+      FCanvas.TextOut(intCurrentXPos, Y, UTF8Copy(strLeft, 1, intEscapePos-1));
+      Inc(intCurrentXPos, FCanvas.TextWidth(UTF8Copy(strLeft, 1, intEscapePos-1)));
+      strLeft := UTF8Copy(strLeft, intEscapePos,
+        UTF8Length(strLeft)-intEscapePos+1);
+
+    end;
+  until Trim(strLeft) = '';
+end;
+
+procedure TConsoleOutput.ProcessCommand(Command: String);
+begin
+  case Command of
+    'High':
+      begin
+        FCanvas.Font.Style := FCanvas.Font.Style + [fsBold];
+        FCanvas.Font.Color := RGBToColor(Min(Red(FCanvas.Font.Color)+
+          FHighlight, 255), Min(Green(FCanvas.Font.Color)+FHighlight, 255),
+          Min(Blue(FCanvas.Font.Color)+FHighlight, 255));
+      end;
+    'Norm', 'Error', 'Warn':
+      begin
+        FCanvas.Font.Style := [];
+        case Command of
+          'Norm': FCanvas.Font.Color := FTextColor;
+          'Warn': FCanvas.Font.Color := FTextWarningColor;
+          'Error': FCanvas.Font.Color := FTextErrorColor;
+        end;
+      end;
+  end;
+end;
+
+constructor TConsoleOutput.Create(ACanvas: TCanvas; AWidth, AHeight: Integer);
+begin
+  FCanvas := ACanvas;
+  FWidth := AWidth;
+  FHeight := AHeight;
+  FBackgroundColor := clBlack;
+  FBackgroundStyle := bsSolid;
+  FTextColor := RGBToColor(0, 200, 0);
+  FTextErrorColor := RGBToColor(200, 0, 0);
+  FTextWarningColor := RGBToColor(200, 200, 0);
+  FHighlight := 55;
+  FTextBuffer := TStringList.Create;
+end;
+
+destructor TConsoleOutput.Free;
+begin
+  FTextBuffer.Free;
+end;
+
+procedure TConsoleOutput.DoDraw;
+begin
+  DrawBackground;
+  DrawTextBuffer;
+  DrawInputLine;
+end;
+
+end.
+
--- src/starter/debugconsole.pas
+++ src/starter/debugconsole.pas
@@ -6,18 +6,22 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  DebugDataHandler, DateUtils;
+  DebugDataHandler, DateUtils, ConsoleComponent, LCLType, Character, LazUTF8;
 
 type
 
   { TfrmDebugConsole }
 
   TfrmDebugConsole = class(TForm)
-    memoDebug: TMemo;
+    Console: TPaintBox;
     pnlConsoleView: TPanel;
+    procedure ConsolePaint(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormUTF8KeyPress(Sender: TObject; var UTF8Key: TUTF8Char);
   private
   protected
+    FConsoleOutput: TConsoleOutput;
     procedure DebugDataHandlerOnNewData(Sender: TObject);
 
   end;
@@ -33,7 +37,42 @@ implementation
 
 procedure TfrmDebugConsole.FormCreate(Sender: TObject);
 begin
+  FConsoleOutput := TConsoleOutput.Create(Console.Canvas, Console.Width,
+    Console.Height);
   gDebugDataHandler.OnNewData := @DebugDataHandlerOnNewData;
+end;
+
+procedure TfrmDebugConsole.FormDestroy(Sender: TObject);
+begin
+  FConsoleOutput.Free;
+end;
+
+procedure TfrmDebugConsole.FormUTF8KeyPress(Sender: TObject;
+  var UTF8Key: TUTF8Char);
+begin
+  case GetUnicodeCategory(UTF8ToUTF16(UTF8Key), 1) of
+    TUnicodeCategory.ucControl:
+      begin
+        case UTF8Key of
+          #8: FConsoleOutput.InputBuffer := UTF8Copy(FConsoleOutput.InputBuffer,
+                1, UTF8Length(FConsoleOutput.InputBuffer)-1);
+          #13:
+            begin
+              //FConsoleOutput.Buffer.Add(FConsoleOutput.InputBuffer);
+              //FConsoleOutput.InputBuffer := '';
+            end;
+        end;
+      end;
+  else
+    FConsoleOutput.InputBuffer := FConsoleOutput.InputBuffer + UTF8Key;
+  end;
+
+  Console.Invalidate;
+end;
+
+procedure TfrmDebugConsole.ConsolePaint(Sender: TObject);
+begin
+  FConsoleOutput.DoDraw;
 end;
 
 procedure TfrmDebugConsole.DebugDataHandlerOnNewData(Sender: TObject);
@@ -45,13 +84,16 @@ begin
     TDebugDataHandler).InfoCount-1];
   case LastInfo.Severity of
     isDebug: strSeverity := 'Debug';
-    isWarning: strSeverity := 'Warning';
-    isError: strSeverity := 'Error';
-    isException: strSeverity := 'Exception';
+    isWarning: strSeverity := '\Warn Warning \Norm';
+    isError: strSeverity := '\Error Error \Norm';
+    isException: strSeverity := '\Error \High Exception \Norm';
   end;
 
-  memoDebug.Append(Format('[%s] %s | %s: %s', [TimeToStr(UnixToDateTime(
+  FConsoleOutput.Buffer.Add(Format('[%s] %s | %s: %s', [TimeToStr(UnixToDateTime(
     LastInfo.Timestamp)), strSeverity, LastInfo.SenderName, LastInfo.Text]));
+
+  //memoDebug.Append(Format('[%s] %s | %s: %s', [TimeToStr(UnixToDateTime(
+  //  LastInfo.Timestamp)), strSeverity, LastInfo.SenderName, LastInfo.Text]));
 end;
 
 end.
